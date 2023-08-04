9bf66a6077428cdba19902e712e74f4ab65fc779
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-04 16:22:44 +0200

    filter done

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,29 @@ import Form from "./components/Form/Form.jsx";
 import { useState } from "react";
 import { uid } from "uid";
 import List from "./components/List /List";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
-  const [isGoodWeather, setIsGoodWeather] = useState(true);
-  const [activities, setActivities] = useState([]);
+  const [isGoodWeather, setIsGoodWeather] = useLocalStorageState(
+    "isGoodWeather",
+    { defaultValue: true }
+  );
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: [],
+  });
 
   function handleAddActivity({ activityInput, isForGoodWeather }) {
-    const goodWeatherActivities = activities.filter(
-      (activity) => activity.isForGoodWeather
-    );
     const newActivity = {
       id: uid(),
       activityText: activityInput,
       isForGoodWeather: isForGoodWeather,
     };
+    console.log(activities);
     setActivities([...activities, newActivity]);
   }
   return (
     <>
-      <List activities={activities}></List>
+      <List activities={activities} isGoodWeather={isGoodWeather}></List>
       <Form handleAddActivity={handleAddActivity} />
     </>
   );
