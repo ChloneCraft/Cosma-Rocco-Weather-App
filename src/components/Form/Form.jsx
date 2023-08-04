50d584df53f10817b03a6849e3b99fbafcc098e5
ChloneCraft <110734347+ChloneCraft@users.noreply.github.com>
2023-08-04 11:54:43 +0200

    implement form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,20 @@
+export default function Form({ onAddActivity }) {
+  function handleSubmitData(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    onAddActivity(data);
+    event.target.reset();
+    document.getElementById("activityInput").focus();
+  }
+  return (
+    <form className="activityForm" onSubmit={handleSubmitData}>
+      <h1 className="title">Add new activity</h1>
+      <label htmlFor="activityInput">Name:</label>
+      <input type="text" id="activityInput" name="activityInput" />
+      <label htmlFor="isForGoodWeather">Good-weather activity: </label>
+      <input type="checkbox" id="isForGoodWeather" name="isForGoodWeather" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
